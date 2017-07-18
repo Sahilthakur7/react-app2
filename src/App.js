3929c3941af404a61b5af967ec258c1325ebc161
SahilThakur7 <sahilrd07@gmail.com>
2017-07-18 17:47:51 +0530

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            kv: "This is kv"
+        }
+    }
+    update(e){
+        this.setState({txt: e.target.value})
+    }
+    render(){
+        return (
+            <div>
+                <h1>Kya</h1>
+                <h2>Hai</h2>
+                {this.props.txt} 
+                {this.props.bunty} 
+                <p>
+                    <input type="text" onChange={this.update.bind(this)}/>
+                    {this.state.txt}
+                </p>
+                <p>
+                    <Button>I <Heart/>React</Button>
+                </p>
+            </div>
+        )
+    }
 }
 
-export default App;
+const Button = (props) => <button>{props.children}</button>
+
+    class Heart extends React.Component {
+        render(){
+            return <span>&hearts;</span>
+        }
+    }
+
+
+App.defaultProps = {
+    bunty: "YO"
+}
+
+export default App
